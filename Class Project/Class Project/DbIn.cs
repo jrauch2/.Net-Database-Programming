23071ddfb00996456b64621a1cc4e4a04bbbc399
Joshua Rauch <jrauch2@my.wctc.edu>
2018-09-01 22:56:30 -0500

    work in progress

--- Class Project/Class Project/DbIn.cs
+++ Class Project/Class Project/DbIn.cs
@@ -4,20 +4,51 @@ using System.Linq;
 
 namespace Class_Project
 {
+    /// <summary>
+    /// The <c>DbIn</c> class.
+    /// Implements the <c>IInput</c> interface.
+    /// Used to read records from a database.
+    /// </summary>
     class DbIn : IInput
     {
         TicketingContext db = new TicketingContext();
+        private string regex = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
 
-        public Ticket findId(int id)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Ticket FindId(int id)
         {
-            throw new System.NotImplementedException();
+            TicketEntity ticketEntity = db.Tickets.Find(id);
+            Ticket ticket = TicketFactory.StringToTicket(ticketEntity.ToString(), regex);
+            return ticket;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int GetMaxID()
         {
-            throw new System.NotImplementedException();
+            int maxId = -1;
+            using (db)
+            {
+                var query = from t in db.Tickets
+                            select t;
+                if (query.Any())
+                {
+                    maxId = db.Tickets.Max(t => t.TicketId);
+                }
+            }
+            return maxId;
         }
 
+        /// <summary>
+        /// Get a list of all tickets stored in the database.
+        /// </summary>
+        /// <returns><c>List<Ticket></c></returns>
         public List<Ticket> GetStoredTickets()
         {
             List<Ticket> storedTickets = new List<Ticket>();
@@ -26,12 +57,13 @@ namespace Class_Project
                 var query = from t in db.Tickets
                             orderby t.TicketId
                             select t;
-                foreach (Ticket ticketEntity in query)
+                foreach (TicketEntity ticketEntity in query)
                 {
-
+                    Ticket ticket = TicketFactory.StringToTicket(ticketEntity.ToString(), regex);
                     storedTickets.Add(ticket);
                 }
             }
+            return storedTickets;
         }
     }
 }
