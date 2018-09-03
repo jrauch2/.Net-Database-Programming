5f3014c0d24dbd00131025907bc49ba9c047d3c0
Joshua Rauch <jrauch2@my.wctc.edu>
2018-09-03 14:47:09 -0500

    Refactored csv manipulation

--- Class Project/Class Project/IOutput.cs
+++ Class Project/Class Project/IOutput.cs
@@ -4,6 +4,10 @@ namespace Class_Project
 {
     internal interface IOutput
     {
+        /// <summary>
+        /// Write out <c>List</c> of <c>Ticket</c> objects to storage medium.
+        /// </summary>
+        /// <param name="tickets">A <c>List</c> of <c>Ticket</c> objects</param>
         void WriteAll(List<Ticket> tickets);
     }
 }
