fa4baab8365847b51e19a1e2e4e4908d7ff8afab
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-08 11:30:15 -0500

    Renamed project to Movie Project

---
+++ Movie Project/Movie Project/Movie Project/IInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Movie_Project
+{/// <summary>
+    /// The <c>IInput</c> interface.
+    /// provides signatures for methods to query stored tickets.
+    /// </summary>
+    internal interface IInput
+    {
+        /// <summary>
+        /// Get a <c>List</c> of all stored movies. 
+        /// </summary>
+        /// <returns>A <c>List</c> of all stored tickets.</returns>
+        List<Movie> GetStoredMovies();
+        /// <summary>
+        /// Gets the highest ID stored.
+        /// Used to generate new tickets without reusing IDs.
+        /// </summary>
+        /// <returns>
+        /// The highest ID as an <c>int</c>.
+        /// </returns>
+        int GetMaxId();
+        /// <summary>
+        /// Get a <c>Ticket</c> by ID.
+        /// </summary>
+        /// <param name="id">The id of the desired <c>Ticket</c> as an <c>int</c>.</param>
+        /// <returns>The desired <c>Ticket</c>, if found.</returns>
+        Movie FindId(int id);
+    }
+}
