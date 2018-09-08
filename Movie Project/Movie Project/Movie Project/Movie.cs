947de3df6f73d764c7d997c84f60aba6a41bfb26
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-08 12:24:59 -0500

    Revert "Revert "Various project files.""
    This reverts commit 035b52d7a77474c72e5deb6e1a28ac0f141783a1.

---
+++ Movie Project/Movie Project/Movie Project/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using NUnit.Framework;
+
+namespace Movie_Project
+{
+    /// <summary>
+    /// The <c>Movie</c> class.
+    /// A <c>Movie</c> consists of an ID, title, and a <c>List</c> of genres.
+    /// </summary>
+    // The Movie class.
+    // A Movie consists of an ID, title, and a List<MovieGenre>.
+    internal class Movie
+    {
+        private int _id;
+        private string _title;
+        private List<MovieGenre> _movieGenres;
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        private void SetId(int id)
+        {
+            this._id = id;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public void SetTitle(string title)
+        {
+            this._title = title;
+        }
+
+        /// <summary>
+        /// Checks if the <c>List</c> already contains the genre, then adds it if it does not.
+        /// </summary>
+        /// <param name="movieGenre"><c>MovieGenre</c> to be added.</param>
+        // Checks if the List<MovieGenre> already contains the genre, then adds it if it does not.
+        public void AddGenre(MovieGenre movieGenre)
+        {
+            if (_movieGenres.Contains(movieGenre))
+                _movieGenres.Add(movieGenre);
+        }
+    }
+}
