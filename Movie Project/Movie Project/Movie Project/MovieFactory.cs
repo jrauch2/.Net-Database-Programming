c95fb1e398ea999378cb7ffed92dc516a53ac8d7
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-09 04:32:24 -0500

    Gave functionality to create tickets.

--- Movie Project/Movie Project/Movie Project/MovieFactory.cs
+++ Movie Project/Movie Project/Movie Project/MovieFactory.cs
@@ -1,4 +1,8 @@
-﻿namespace Movie_Project
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NLog;
+
+namespace Movie_Project
 {
     /// <summary>
     /// The <c>MovieFactory</c> Class.
@@ -10,7 +14,11 @@
     // Builds all Movie objects.
     internal sealed class MovieFactory
     {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private static readonly MovieFactory Instance = new MovieFactory();
+        private static readonly GenreTracker GenreTrackerInstance = GenreTracker.GetGenreTrackerInstance();
+        private const string Rx = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+        private const char GenreSplit = '|';
 
         /// <summary>
         /// Private constructor.
@@ -31,6 +39,44 @@
         public static MovieFactory GetMovieFactoryInstance()
         {
             return Instance;
+        }
+
+        public Movie NewMovie(int id, string title, List<string> genres)
+        {
+            return new Movie(id, title, genres);
+        }
+
+        public Movie NewMovie(string movieString)
+        {
+            var sa = Regex.Split(movieString, Rx);
+            var id = int.Parse(sa[0]);
+            var title = sa[1];
+            var genres = ParseMovieGenres(sa[2]);
+            return new Movie(id, title, genres);
+        }
+
+        private List<string> ParseMovieGenres(string genreString)
+        {
+            var list = new List<string>();
+            if (genreString != null)
+            {
+                var sa = genreString.Split(GenreSplit);
+                foreach (var s in sa)
+                {
+                    if (GenreTrackerInstance.GetAllMovieGenres().Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                    else
+                    {
+                        GenreTrackerInstance.AddGenre(s);
+                        list.Add(s);
+                    }
+                }
+            }
+            list.Sort();
+
+            return list;
         }
     }
 }
