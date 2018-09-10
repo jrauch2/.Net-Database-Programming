c9685f4912186173254695416b117041a920b153
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-10 10:59:05 -0500

    Removed GetStoredMovies(). Added FindMovieById().

--- Movie Project/Movie Project/Movie Project/IInput.cs
+++ Movie Project/Movie Project/Movie Project/IInput.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections.Generic;
-
-namespace Movie_Project
+﻿namespace Movie_Project
 {/// <summary>
     /// The <c>IInput</c> interface.
     /// provides signatures for methods to query stored movies.
     /// </summary>
     internal interface IInput
     {
-        /// <summary>
-        /// Get a <c>List</c> of all stored movies. 
-        /// </summary>
-        /// <returns>A <c>List</c> of all stored movies.</returns>
-        List<Movie> GetStoredMovies();
+//        /// <summary>
+//        /// Get a <c>List</c> of all stored movies. 
+//        /// </summary>
+//        /// <returns>A <c>List</c> of all stored movies.</returns>
+//        List<Movie> GetStoredMovies();
+        
         /// <summary>
         /// Gets the highest ID stored.
         /// Used to generate new movies without reusing IDs.
@@ -20,11 +19,25 @@ namespace Movie_Project
         /// The highest ID as an <c>int</c>.
         /// </returns>
         int GetMaxId();
+
         /// <summary>
         /// Get a <c>Movie</c> by ID.
         /// </summary>
         /// <param name="id">The id of the desired <c>Movie</c> as an <c>int</c>.</param>
+        /// <param name="movie"></param>
         /// <returns>The desired <c>Movie</c>, if found.</returns>
-        Movie FindId(int id);
+        bool FindMovieById(int id, out Movie movie);
+
+        /// <summary>
+        /// Get a <c>Movie</c> by title.
+        /// </summary>
+        /// <param name="title">The title of the desired <c>Movie</c></param>
+        /// <param name="movie">The <c>Movie</c> wanted</param>
+        /// <returns><c>true</c> if movie is found, <c>false</c> if not.</returns>
+        bool FindMovieByTitle(string title, out Movie movie);
+
+        bool Contains(Movie movie);
+
+        void AddMovie(Movie movie);
     }
 }
