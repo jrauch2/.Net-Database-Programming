68778b143cabbf6a6f6f92c5890b55595d75c7eb
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-10 11:03:31 -0500

    Remove unused references. Added comments.

--- Movie Project/Movie Project/Movie Project/MovieFactory.cs
+++ Movie Project/Movie Project/Movie Project/MovieFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using NLog;
 
 namespace Movie_Project
 {
@@ -14,18 +13,14 @@ namespace Movie_Project
     // Builds all Movie objects.
     internal sealed class MovieFactory
     {
-        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private static readonly MovieFactory Instance = new MovieFactory();
         private static readonly GenreTracker GenreTrackerInstance = GenreTracker.GetGenreTrackerInstance();
-        private const string Rx = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
         private const char GenreSplit = '|';
 
         /// <summary>
         /// Private constructor.
-        /// To access <c>MovieFactory</c>, call <c>GetMovieFactoryInstance()</c>.
         /// </summary>
         // Private constructor.
-        // To access MovieFactory, call GetMovieFactoryInstance().
         private MovieFactory()
         {
 
@@ -34,44 +29,67 @@ namespace Movie_Project
         /// <summary>
         /// Get the <c>MovieFactory</c> instance.
         /// </summary>
-        /// <returns>THe <c>MovieFactory</c> instance</returns>
+        /// <returns>The <c>MovieFactory</c> instance</returns>
         // Get the MovieFactory instance.
         public static MovieFactory GetMovieFactoryInstance()
         {
             return Instance;
         }
 
+        /// <summary>
+        /// Generate a new movie.
+        /// This method is overloaded.
+        /// </summary>
+        /// <param name="id">an int representing the id of the ticket.</param>
+        /// <param name="title">a <c>string</c> of the movie's title</param>
+        /// <param name="genres">a <c>List</c> of genres</param>
+        /// <returns>The generated movie.</returns>
+        // Generate a new movie.
+        // This method is overloaded.
+        // This takes an int id, string title, and List<string> genres.
         public Movie NewMovie(int id, string title, List<string> genres)
         {
             return new Movie(id, title, genres);
         }
 
-        public Movie NewMovie(string movieString)
+        /// <summary>
+        /// Generate a new movie.
+        /// This method is overloaded.
+        /// </summary>
+        /// <param name="movieString">a <c>string</c> of the movie's properties.</param>
+        /// <param name="regularExpression">a <c>string</c> of a regular expression for parsing the <c>movieString</c></param>
+        /// <returns>The generated movie.</returns>
+        // Generate a new movie.
+        // This method is overloaded.
+        // This takes an string of the movie's properties and a regular expression string.
+        public Movie NewMovie(string movieString, string regularExpression)
         {
-            var sa = Regex.Split(movieString, Rx);
+            var sa = Regex.Split(movieString, regularExpression);
             var id = int.Parse(sa[0]);
             var title = sa[1];
-            var genres = ParseMovieGenres(sa[2]);
+            var genres = ParseMovieGenres(sa[2], GenreSplit);
             return new Movie(id, title, genres);
         }
 
-        private List<string> ParseMovieGenres(string genreString)
+        /// <summary>
+        /// Parse the string of genres, delimited by the <c>char</c> argument.
+        /// </summary>
+        /// <param name="genreString">The string to be parsed</param>
+        /// <param name="genreSplit">The <c>char</c> to split the string on.</param>
+        /// <returns>A <c>List</c> of genres.</returns>
+        // Parse the string of genres, delimited by the char argument.
+        public static List<string> ParseMovieGenres(string genreString, char genreSplit)
         {
             var list = new List<string>();
             if (genreString != null)
             {
-                var sa = genreString.Split(GenreSplit);
-                foreach (var s in sa)
+                var sa = genreString.Split(genreSplit);
+                foreach (var g in sa)
                 {
-                    if (GenreTrackerInstance.GetAllMovieGenres().Contains(s))
-                    {
-                        list.Add(s);
-                    }
-                    else
-                    {
-                        GenreTrackerInstance.AddGenre(s);
-                        list.Add(s);
-                    }
+                    var t = g.Trim();
+                    if (!GenreTrackerInstance.Contains(t.FirstCharToUpper()))
+                    GenreTrackerInstance.AddGenre(t.FirstCharToUpper());
+                    list.Add(t);
                 }
             }
             list.Sort();
