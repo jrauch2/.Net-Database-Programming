122565e2f0cdc1fae5a5e2d4158e695d0301ccd7
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-10 11:04:04 -0500

    Removed all testing.

--- Movie Project/Movie Project/Movie Project/Program.cs
+++ Movie Project/Movie Project/Movie Project/Program.cs
@@ -1,68 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Core.Mapping;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using NLog;
-using NUnit.Framework.Api;
-
-namespace Movie_Project
+﻿namespace Movie_Project
 {
     internal class Program
     {
+        private static readonly MovieProject MovieProjectInstance = MovieProject.GetMovieProjectInstance();
         public static void Main(string[] args)
         {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Trace("Application starting...");
-            //            const string regex = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-            //            var genres = new List<string>();
-            //            using (var file = new StreamReader("ml-latest-small\\movies.csv"))
-            //            {
-            //                var count = 0;
-            //                while (!file.EndOfStream)
-            //                {
-            //                    if (count++ == 0)
-            //                    {
-            //                        file.ReadLine();
-            //                    }
-            //                    else
-            //                    {
-            //                        var line = file.ReadLine();
-            //                        if (line == null) continue;
-            //                        var movie = Regex.Split(line, regex);
-            //                        var genreArray = movie[2].Split('|');
-            //
-            //                        genres.Sort();
-            //
-            //                        foreach (var s in genreArray)
-            //                        {
-            //                            if (!genres.Contains(s))
-            //                            {
-            //                                genres.Add(s);
-            //                            }
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //
-            //            using (var fileOut = new StreamWriter("genres.csv"))
-            //            {
-            //                var count = 0;
-            //                for (var i = 0; i < genres.Count(); i++) 
-            //                {
-            //                    if (count++ == 0)
-            //                        fileOut.Write(genres[i]);
-            //                    else
-            //                    {
-            //                        fileOut.Write(',' + genres[i]);
-            //                    }
-            //                }
-            //            }
-
-
+            MovieProjectInstance.Menu();
         }
     }
 }
