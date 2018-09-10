f622e5bfcd3c6f6dd612c5af48dbb5a8c528fe69
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-09-10 11:00:39 -0500

    Removed WriteAll(). Added StoreMovies();

--- Movie Project/Movie Project/Movie Project/IOutput.cs
+++ Movie Project/Movie Project/Movie Project/IOutput.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections.Generic;
-
-namespace Movie_Project
+﻿namespace Movie_Project
 {
     internal interface IOutput
     {
         /// <summary>
         /// Write out <c>List</c> of <c>Movie</c> objects to storage medium.
         /// </summary>
-        /// <param name="movies">A <c>List</c> of <c>Movie</c> objects</param>
         // Write out a List<Movie> to storage medium.
-        void WriteAll(List<Movie> movies);
+        void StoreMovies();
     }
 }
